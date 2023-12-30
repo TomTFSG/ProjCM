9cbb78af388a9e17b3be2f02412ddeb38bb2e8f9
TomTFSG <tommatiasferreira@gmail.com>
2023-12-30 18:08:42 +0000

    Menu

--- app/src/main/java/com/example/projeto/Menu.java
+++ app/src/main/java/com/example/projeto/Menu.java
@@ -7,12 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class Menu extends Fragment {
 
     public Menu() {
-        // Required empty public constructor
+
     }
 
 
@@ -25,6 +26,42 @@ public class Menu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view=getView();
+        Button humido=view.findViewById(R.id.humido);
+        Button neutro=view.findViewById(R.id.neutro);
+        Button seco=view.findViewById(R.id.seco);
+
+        humido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame, Humido.class,null)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+        neutro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame, Neutro.class,null)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+        seco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame, Seco.class,null)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_menu, container, false);
     }
 }
